53a74c14c2070a2f33f90a832b92e107f2a913e0
Basil <kaffeetrinker+github@gmail.com>
2018-03-29 22:55:08 +0200

    added docker-compose configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    mailjetEndpoint: 'https://api.mailjet.com/v3.1/send'
+    mailjetEndpoint: 'https://api.mailjet.com/v3.1/send',
+    postgrestUrl: 'http://localhost:4000/api'
 };
